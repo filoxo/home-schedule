78f8863f1ffc862224f9f50bff1dfaadd5ae005a
Carlos Filoteo <c.filoteo@live.com>
2017-07-26 03:49:12 -0600

    Add and export Data

---
+++ src/Data.tsx
@@ -0,0 +1,40 @@
+export interface Day {
+    day: string;
+    tasks: string[];
+}
+
+const Schedule: Day[] = [{
+    day: 'Monday', 
+    tasks: [
+        'Wipe Kitchen counters',
+        'Load dishwasher',
+        'Take out trash'
+    ]
+}, {
+    day: 'Tuesday', 
+    tasks: [
+        'test'
+    ]
+}, {
+    day: 'Wednesday', 
+    tasks: [
+        'test'
+    ]
+}, {
+    day: 'Thursday', 
+    tasks: [
+        'test'
+    ]
+}, {
+    day: 'Friday', 
+    tasks: [
+        'test'
+    ]
+}, {
+    day: 'Saturday', 
+    tasks: [
+        'test'
+    ]
+}];
+
+export default Schedule;
