f2cccb6ed2fd4e08bd88f3db20374177f38a2a7b
Carlos Filoteo <c.filoteo@live.com>
2017-07-26 03:08:14 -0600

    Add material tabs based on example

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,77 @@
 import React, { Component } from 'react';
 import './App.css';
 
-const logo = require('./logo.svg');
+import { withStyles, createStyleSheet } from 'material-ui/styles';
+import SwipeableViews from 'react-swipeable-views';
+import AppBar from 'material-ui/AppBar';
+import Tabs, { Tab } from 'material-ui/Tabs';
 
-class App extends Component<{}, {}> {
+const TabContainer = ({children}: {children: React.ReactNode}) => (
+  <div style={{ padding: 24 }}>
+    {children}
+  </div>
+);
+
+const styleSheet = createStyleSheet('FullWidthTabs', theme => ({
+  root: {
+    backgroundColor: theme.palette.background.paper,
+  }
+}));
+
+export interface AppProps { classes: string; }
+export interface AppState { index: string; }
+
+class App extends Component<AppProps, {index: number}> {
+  state = { index: 0 };
+  handleChange = (event: React.ChangeEvent<{checked: boolean}>, index: number) => {
+    this.setState({ index });
+  }
+  handleChangeIndex = (index: number) => {
+    this.setState({ index });
+  }
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+      <div>
+        <AppBar position="static" color="default">
+          <Tabs
+            index={this.state.index}
+            onChange={this.handleChange}
+            indicatorColor="primary"
+            textColor="primary"
+            scrollable={true}
+            scrollButtons="auto"
+          >
+            <Tab label="Monday" />
+            <Tab label="Tuesday" />
+            <Tab label="Wednesday" />
+            <Tab label="Thursday" />
+            <Tab label="Friday" />
+            <Tab label="Saturday" />
+          </Tabs>
+        </AppBar>
+        <SwipeableViews index={this.state.index} onChangeIndex={this.handleChangeIndex}>
+          <TabContainer>
+            {'Item One'}
+          </TabContainer>
+          <TabContainer>
+            {'Item Two'}
+          </TabContainer>
+          <TabContainer>
+            {'Item Three'}
+          </TabContainer>
+          <TabContainer>
+            {'Item Four'}
+          </TabContainer>
+          <TabContainer>
+            {'Item Five'}
+          </TabContainer>
+          <TabContainer>
+            {'Item Six'}
+          </TabContainer>
+        </SwipeableViews>
       </div>
     );
   }
 }
 
-export default App;
+export default withStyles(styleSheet)(App);
